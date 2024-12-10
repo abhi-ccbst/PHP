ec80d7569e23da985dd0a1e3f000d45943c9912d
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2024-12-10 18:53:35 -0500

    Lec-1

--- Lecture-1/lecture1.php
+++ Lecture-1/lecture1.php
@@ -9,11 +9,43 @@
     This is multi line comment.
 */ 
 $x = 5;
-$y = "Abhi";
-echo $y;
+$float = 10.4;
+$bool = true;
+$y = "Abhi Patel";
+ECHO var_dump($x);
+echo var_dump($bool);
 echo "<br>";
-echo "I am Abhi";
+echo "<h2>I am Abhi $x</h2>";
+// Print and echo are same thing..
+print "<h2>I am Abhi $x</h2>";
+print '<h2>I am Abhi $x</h2>';
+echo strlen($y);
+echo "<br>";
+echo str_word_count("$y");
+echo "<br>";
+echo strpos($y, "Patel");
 ?>
 
 </body>
 </html>
+
+<!-- 
+Data types: 
+    String
+    Integer
+    Float (floating point numbers - also called double)
+    Boolean
+    Array
+    Object
+    NULL
+    Resource
+
+var_dump(): You can get the data type of any object by using the var_dump() function.
+
+
+*** String
+strlen(): The PHP strlen() function returns the length of a string.
+str_word_count(): The PHP str_word_count() function counts the number of words in a string.
+strpos(): The PHP strpos() function searches for a specific text within a string.
+
+-->
