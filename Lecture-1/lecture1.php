4f6660b22886b0c70165223d6815aa463f3d7545
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2024-12-10 18:22:38 -0500

    Lec-1

--- Lecture-1/lecture1.php
+++ Lecture-1/lecture1.php
@@ -3,8 +3,16 @@
 <body>
 
 <?php
-$txt = "PHP";
-echo "I love $txt!";
+// This is single-line comment
+# This is also a single-line comment 
+/*
+    This is multi line comment.
+*/ 
+$x = 5;
+$y = "Abhi";
+echo $y;
+echo "<br>";
+echo "I am Abhi";
 ?>
 
 </body>
