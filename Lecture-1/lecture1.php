34e078bcae75dea8d1bebabc406bc97d484852e0
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2024-12-11 17:48:09 -0500

    Lect-2

--- Lecture-1/lecture1.php
+++ Lecture-1/lecture1.php
@@ -38,8 +38,19 @@ echo "<br>";
 echo trim(" Abhi Patel ");
 
 $full= $a . " " . $p;
-$full = "$a $p";
+$full = "$a $p test";
 echo "<br>$full";
+echo "<br>";
+// full: Abhi Patel test
+echo substr($full, 5);
+echo "<br>";
+echo substr($full, -4);
+
+$x = 1;
+$int_cat= (string)$x;
+echo var_dump($int_cat);
+
+echo rand(1, 100);
 ?>
 
 </body>
@@ -68,6 +79,27 @@ strtolower(): The strtolower() function returns the string in lower case:
 str_replace(): The PHP str_replace() function replaces some characters with some other characters in a string.
 strrev(): The PHP strrev() function reverses a string.
 trim(): The trim() removes any whitespace from the beginning or the end:
+substr():  can return a range of characters by using the substr() function.
 
+boolean: 
+var_dump(is_float($x))
+var_dump(is_int($x))
+var_dump(is_numeric($x))
 
+Cast:
+(string) - Converts to data type String
+(int) - Converts to data type Integer
+(float) - Converts to data type Float
+(bool) - Converts to data type Boolean
+(array) - Converts to data type Arra
+
+Math: 
+pi(): 3.14
+min(1,2,3,4,5)
+max(1,2,3,4,5)
+sqrt(4)
+round()
+rand()
+rand(1)
+rand(1, 100)
 -->
