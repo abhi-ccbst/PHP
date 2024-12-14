0f883d9b1c346b9a43944ede5588b7bf76790ba0
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2024-12-13 20:02:09 -0500

    Lec-3

--- Lecture-3/index.php
+++ Lecture-3/index.php
@@ -1,43 +1,47 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form method="post" action="welcome.php">
+    <!-- action="<?php echo $_SERVER['PHP_SELF'];?>"> -->
     Name: <input type="text" name="fname">
+    Email: <input type="email" name="femail" require="true">
     <input type="submit">
 </form>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //Collect data from input field
-    $name = htmlspecialchars($_REQUEST['fname']);
-    if (empty($name)) {
-        echo "Name is empty";
-    } else {
-        echo "Good Morning $name";
-    }
-}
+<?php 
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     //Collect data from input field
+//     $name = htmlspecialchars($_REQUEST['fname']);
+//     $email = htmlspecialchars($_REQUEST['femail']);
+//     if (empty($name)) {
+//         echo "Name is empty";
+//     } else {
+//         echo "Good Morning $name";
+//     }
+//     echo "Email: $email";
+// }
 
-$x = 100;
-function printX() {
-    // echo $x;
-    echo $GLOBALS['x'];
-}
+// $x = 100;
+// function printX() {
+//     // echo $x;
+//     echo $GLOBALS['x'];
+// }
 
-function printServer(){
-    // foreach ($_SERVER as $key => $val) {
-    //     echo "<br>" . $key . " " . $_SERVER['PHP_SELF'];
-    // }
+// function printServer(){
+//     // foreach ($_SERVER as $key => $val) {
+//     //     echo "<br>" . $key . " " . $_SERVER['PHP_SELF'];
+//     // }
 
-    echo "<br>PHP_SELF " . $_SERVER['PHP_SELF'];
-    echo "<br>SERVER_NAME " . $_SERVER['SERVER_NAME'];
-    echo "<br>HTTP_HOST " . $_SERVER['HTTP_HOST'];
-    echo "<br>HTTP_REFERER " . $_SERVER['HTTP_REFERER'];
-    echo "<br>HTTP_USER_AGENT " . $_SERVER['HTTP_USER_AGENT'];
-    echo "<br>SCRIPT_NAME " . $_SERVER['SCRIPT_NAME'];
-}
+//     echo "<br>PHP_SELF " . $_SERVER['PHP_SELF'];
+//     echo "<br>SERVER_NAME " . $_SERVER['SERVER_NAME'];
+//     echo "<br>HTTP_HOST " . $_SERVER['HTTP_HOST'];
+//     echo "<br>HTTP_REFERER " . $_SERVER['HTTP_REFERER'];
+//     echo "<br>HTTP_USER_AGENT " . $_SERVER['HTTP_USER_AGENT'];
+//     echo "<br>SCRIPT_NAME " . $_SERVER['SCRIPT_NAME'];
+// }
 
-printX();
-printServer();
+// printX();
+// printServer();
 
 ?>
 </body>
