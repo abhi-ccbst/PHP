a8dad740e0ae3c949c77aa014727b19800f85a57
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2024-12-18 17:56:31 -0500

    InClass-5

--- InClass-5/index.php
+++ InClass-5/index.php
@@ -4,7 +4,7 @@
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload.
-    <input type="file" name="fileUploaded" id = "fileUploaded">
+    <input type="file" name="fileUploaded" id = "fileUploaded" required>
     <input type="submit" name = "submit">
     
 </form>
--- InClass-5/upload.php
+++ InClass-5/upload.php
@@ -5,6 +5,8 @@
 <?php 
     $target_dir = "/Applications/XAMPP/xamppfiles/htdocs/PHP/InClass-5/";
     $target_file = $target_dir . basename($_FILES["fileUploaded"]["name"]);
+    $uploadOk = 1;
+    // If $uploadOk is 1 when success or else fail
     foreach ( $_FILES["fileUploaded"] as $key => $val) {
         echo "$key = $val <br>";
     }
@@ -18,11 +20,34 @@
             echo "File is an image";
         } else {
             echo "File is not an image";
+            $uploadOk = 0;
         }
     }
 
+    if (file_exists($target_file)) {
+        echo "File already exists";
+        $uploadOk = 0;
+    }
+
+    if ($imageFileType != "jpg" && $imageFileType != "png") {
+        echo "Only JPG and PNG files are allowed.";
+        $uploadOk = 0;
+    }
+
+    if ($uploadOk == 0) {
+        echo "your file was not uploaded";
+    } else  {
+        if (move_uploaded_file($_FILES["fileUploaded"]["tmp_name"], $target_file)) {
+            echo "<strong>The file " . htmlspecialchars(basename($_FILES["fileUploaded"]["name"])) . " has been uploaded.</strong>";
+        } else {
+            echo "Ther was an error uploading your file.";
+        }
     
-?>
+    }
+
+
+
+?>  
 
 </body>
 </html>
