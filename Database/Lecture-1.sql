ed371df953c6346bcbe70b8f1114c024fe24eb87
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-01-06 20:16:09 -0500

    Added Database lec-1.sql

--- Database/Lecture-1.sql
+++ Database/Lecture-1.sql
@@ -1,3 +1,5 @@
+SET SQL_SAFE_UPDATES = 0;
+
 show databases;
 
 DROP DATABASE IF EXISTS ccbst;
@@ -18,7 +20,8 @@ CREATE TABLE ccbst.Persons (
 
 INSERT INTO ccbst.Persons (personID, lastName, firstName, address) 
 VALUES (1, 'patel', 'Abhi', 'Canada'),
-(2, 'wick', 'John', 'Canada');
+(2, 'wick', 'John', 'Canada'),
+(3, 'lastName', 'firstName', NULL);
 
 SELECT * FROM ccbst.Persons WHERE lastName = 'wick' AND personID = 2;
 
@@ -29,3 +32,13 @@ SELECT * FROM ccbst.Persons WHERE NOT lastName = 'wick' AND NOT personID = 1;
 SELECT * FROM ccbst.Persons ORDER BY personID ;
 
 SELECT * FROM ccbst.Persons ORDER BY personID DESC;
+
+SELECT * FROM ccbst.Persons WHERE address IS NULL;
+
+SELECT * FROM ccbst.Persons WHERE address IS NOT NULL;
+
+UPDATE ccbst.Persons SET lastName = 'McCarty', firstName = 'Lilliana' WHERE personID = 3;
+
+DELETE FROM ccbst.Persons WHERE personID = 1;
+
+SELECT * FROM ccbst.Persons;
